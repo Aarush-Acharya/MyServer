2be464f0eb84777fc653d67f86bda50d581fcae3
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2023-07-04 02:50:47 +0530

    Update MyServer.py

--- MyServer.py
+++ MyServer.py
@@ -18,7 +18,7 @@ def onhook():
     body = request.json
     print("Hey "+ body['sender']['login'] +" interacted with a new "+ str(list(body.values())[0]) + " "+  str(list(body.keys())[1]) + " in "+ body['repository']['name'] + " belonging to "+  body['repository']['owner']['login'])
     webhook = Webhook.from_url("https://discord.com/api/webhooks/1125510554585419828/lrwnSYeYQuvYHLtT4X5W5eYVMqi5J1802PfAc5WYf9VpyVQE5GI9QKKSzr4yIZCR6jmN", adapter=RequestsWebhookAdapter())
-    webhook.send("Hey "+ body['sender']['login'] +" did a new "+ str(list(body.values())[0]) + " "+  str(list(body.keys())[1]) + " in "+ body['repository']['name']+" belonging to "+  body['repository']['owner']['login'])
+    webhook.send("Hey "+ body['sender']['login'] +" interacted with a new "+ str(list(body.values())[0]) + " "+  str(list(body.keys())[1]) + " in "+ body['repository']['name']+" belonging to "+  body['repository']['owner']['login'])
     return jsonify({'body': body})
     # payload = body.getlist('payload')
     # print(payload)
